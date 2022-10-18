49b5adb7f1518c7f39c80c100166d9b389f9a191
dian <dian.nuraeni.tkom419@polban.ac.ic>
2022-10-18 16:33:35 +0700

    Tugas1AndroidGradle

---
+++ app/src/main/java/com/example/kt1/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.kt1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kt1.ui.theme.KT1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KT1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("DIAN NURAENI")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KT1Theme {
+        Greeting("Android")
+    }
+}
